68b5d465ffe754a2ae7f1ae2ebe39f18d430aa9f
Amin Abdisamad <theaminux@gmail.com>
2018-03-30 08:43:30 +0300

    Display variables

---
+++ variables.py
@@ -0,0 +1,37 @@
+# Variable and data types
+greeting = "Hi, every body"
+# print(greeting)
+
+# Data types
+
+# String
+str = 'Salaam'
+
+# Numbers
+num = 34
+# Float
+myfloat = 1.4
+
+# Lists - its like one dimentional array
+mylist = [1, 3, 4, 5, ' Hello', 1.5]
+
+# Dictionaries - its like assosiative array in php or objects in JS
+mydict = {
+    "id": 1,
+    "name": "Amin Abdisamad",
+    "age": 27,
+    "address": {
+        "Street": "30 Street Blacksea",
+        "City": "Mogadishu"
+    }
+
+}
+
+# Display the variables above - will use (type)
+print(type(str), str)
+print(type(num), num)
+print(type(myfloat), myfloat)
+print(type(mydict), mydict)
+
+
+
