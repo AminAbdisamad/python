350754d3aab89c2cdac156b3009d89ae8c963f76
Amin Abdisamad <theaminux@gmail.com>
2018-03-30 09:19:32 +0300

    Added while loop

--- loops.py
+++ loops.py
@@ -1,1 +1,26 @@
-# Loops
+# For Loops
+
+People = ['Amin', 'Abdihalim', 'Ali', 'Hassan']
+for person in People:
+    print('Current Person: ', person)
+
+# Iterative by sequence
+for i in range(len(People)):
+    print(People[i])
+
+# for i in range(0, 10):
+#     print(i)
+
+# While Loops
+count = 0
+while count <= 20:
+    print(count)
+    count +=1
+
+# Breaking While Loops
+count = 0
+while count <= 20:
+    if count == 10:
+        break
+    print(count)
+    count = count + 1
