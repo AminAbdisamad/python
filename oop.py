07901e7bcec319e7e447dc22eb4f725ecb41176f
Amin Abdisamad <theaminux@gmail.com>
2018-04-01 16:16:41 +0300

    updated oop.py

--- oop.py
+++ oop.py
@@ -21,6 +21,9 @@ class Person:
     def get_email(self):
         return self.__email
 
+    def tostring(self):
+        return '{} can be contacted at {}'.format(self.__name, self.__email)
+
 
 aminux = Person('Amin', 'aminux@gmail.com')
 # aminux.set_name('Mohamed Amin')
@@ -28,5 +31,36 @@ aminux = Person('Amin', 'aminux@gmail.com')
 
 print(aminux.get_name())
 print(aminux.get_email())
+
+print(aminux.tostring())
+
+
+# Create new Customer class to extend person
+class Customer(Person):
+    __balance = 0
+
+    def __init__(self, name, email, balance):
+        self.__name = name
+        self.__email = email
+        self.__balance = balance
+        super(Customer, self).__init__(name, email)
+
+    def set_balance(self, balance):
+        self.__balance = balance
+
+    def get_balance(self):
+        return self.__balance
+
+    def customer_info(self):
+        return '{} has a balance of {} and can be contacted {}'.format(self.__name, self.__balance, self.__email)
+
+
+Abdihalim = Customer('Abdihalim', 'abdihalim@gmail.com', 100)
+
+print(Abdihalim.customer_info())
+
+
+
+
 
 
